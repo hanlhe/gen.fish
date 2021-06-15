54d1dc6df00f3b8a4a3d3ea63cacfbf188c337fc
Hanlin He <hanlhe@cisco.com>
2021-06-14 17:10:47 -0700

    jdk

--- functions/jdk.fish
+++ functions/jdk.fish
@@ -1,5 +1,6 @@
 function jdk --description 'Set alternative JDK version' --argument-names ver
     echo "Set JDK to version $ver"
-    set -x JAVA_HOME (/usr/libexec/java_home -v $ver)
+	set java_version $argv
+	set -Ux JAVA_HOME (/usr/libexec/java_home -v $java_version)
     java -version
 end
