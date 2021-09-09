453e4d12fdae52481d14a09c7bc1f82f19ba7a2d
Hanlin He <hanlhe@cisco.com>
2021-09-09 11:35:46 -0700

    Remove exa.

--- conf.d/exa.fish
+++
@@ -1,6 +0,0 @@
-# general use
-alias l='exa -lbF --git'                                               # list, size, type, git
-alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
